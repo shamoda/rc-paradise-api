a3dd72dbacd83088d2b76ccad2e53a8d8aa6f89e
shamoda <it19161648@my.sliit.lk>
2021-04-28 20:00:28 +0530

    initial commit

---
+++ src/main/java/com/api/rc_paradise_api/RcParadiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.rc_paradise_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcParadiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcParadiseApiApplication.class, args);
+	}
+
+}
